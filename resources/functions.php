f41f499eabd8bee3e75b76e2672595fe020a638d
pmward <pward@paypal.com>
2015-09-14 09:25:55 +0100

    initial project setup

--- resources/functions.php
+++ resources/functions.php
@@ -1,6 +1,44 @@
 <?php
 
+function redirect() {
+    
+    header("Location: $location ");
+
+}
+
+function query() {
+    
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
 
 
+function confirm($result) {
+    
+    global $connection;
+    
+    if(!$result) {
+        die("QUERY FAILED " . mysqli_error($connection));
+    
+    }
+}
+
+
+function escape_string($string) {
+    
+    global $connection;
+    return mysqli_query($connection, $string);
+}
+
+
+function fetch_array($result) {
+    
+    return mysqli_fetch_array($result)
+
+}
+    
+
+
+}
 
 ?>
